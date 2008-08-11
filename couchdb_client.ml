37976e5d5137e6f48ceee621d3ae216af329dbed
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2008-08-11 21:49:46 +0200

    More flesh.

--- couchdb_client.ml
+++ couchdb_client.ml
@@ -5,6 +5,7 @@ module Http_method =
     type t = | Get 
 	     | Put of Json_type.t 
 	     | Post of (string * string) list
+	     | Post_raw of Json_type.t
 	     | Delete
 
     let to_http_call m path =
@@ -13,6 +14,9 @@ module Http_method =
 	| Put json ->
 	    let str = Json_io.string_of_json ~compact:true json in
 	      new Http_client.put path str
+	| Post_raw json ->
+	    let str = Json_io.string_of_json ~compact:true json in
+	      new Http_client.post_raw path str
 	| Post headers -> new Http_client.post path headers
 	| Delete -> new Http_client.delete path
   end
@@ -46,25 +50,47 @@ let l_request m =
     pipe_empty := false;
     p # run ()
 
-let request mthod path content headers =
+let request ?(content=None) ?(headers=[]) mthod path =
   let call = Http_method.to_http_call mthod path in
-  let headers =
+  let augmented_headers =
+    headers @
     ["Accept", "application/json"] @
     match mthod with
       | Http_method.Put _ -> ["Content-Type", "application/json"]
       | _ -> [] in
-  let header = new Netmime.basic_mime_header ~ro:true headers in
+  let header = new Netmime.basic_mime_header ~ro:true augmented_headers in
     call#set_request_header header;
-    l_request call
+    l_request call;
+    call
       
-let get db doc_id = Json_type.Null
+let request_with_db ?(content=None) ?(headers=[]) db m components =
+  let {server = _s; database = _db} = db in
+  let build_url components = "" in
+    request m (build_url components)
 
-let create db json = 0
+let get db doc_id =
+  let r = request_with_db db Http_method.Get [doc_id] in
+    Json_io.json_of_string (r # get_resp_body())
 
-let delete db doc_id = ()
-let update db doc_id json = ()
-let info db = Json_type.Null
+let mk_doc_id i = i
 
+let create db json =
+  let r = request_with_db db (Http_method.Post_raw json) [] in
+    mk_doc_id (int_of_string (r # get_resp_body()))
+
+let delete db doc_id =
+  let _r = request_with_db db Http_method.Delete [doc_id] in
+    () (* TODO: Handle errors! *)
+
+let update db doc_id json =
+  let _r = request_with_db db (Http_method.Put json) [doc_id] in
+    () (* TODO: Handle response! *)
+
+let info db =
+  let r = request_with_db db Http_method.Get [] in
+    Json_io.json_of_string (r # get_resp_body())
+
+(* Strictly TODO *)
 let query db mapper reducer init = init
 
 
