3ed80c74f378e23d8376265009be97ae59e2db48
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2008-08-11 20:30:40 +0200

    Flesh out a simple simple API to start with.

--- couchdb_client.ml
+++ couchdb_client.ml
@@ -1,3 +1,25 @@
 (* CouchDB Client implements a client for the CouchDB Document storage *)
 
-type t = int
+type t = {url: string}
+
+let mk_server url = {url = url}
+
+type db = {server: t;
+	   database: string}
+
+type doc_id = int
+
+let mk_database server db = {server = server;
+			     database = db}
+				 
+let get db url = Json_type.Null
+
+let create db json = 0
+
+let delete db doc_id = ()
+let update db doc_id json = ()
+let info db = Json_type.Null
+
+let query db mapper reducer init = init
+
+
--- couchdb_client.mli
+++ couchdb_client.mli
@@ -1,3 +1,39 @@
 (* Interface for the CouchDB Document storage client *)
 
 type t
+  (** Represents a CouchDB server *)
+type db
+  (** Represents a CouchDB database *)
+
+type doc_id
+  (** Documentation identifiers *)
+
+val mk_server : string -> t
+  (** Create a new CouchDB representation object *)
+
+val mk_database : t -> string -> db
+  (** Create a Database from a server and a name *)
+
+val get : db -> doc_id -> Json_type.json_type
+  (** Lowlevel GET *)
+
+val create : db -> Json_type.json_type -> doc_id
+  (** Lowlevel CREATE *)
+
+val delete : db -> doc_id -> unit
+  (** Lowlevel DELETE *)
+
+val update : db -> doc_id -> Json_type.json_type -> unit
+  (** Lowlevel UPDATE *)
+
+val info : db -> Json_type.json_type
+  (** Return information about Database *)
+
+val query : db
+  -> (Json_type.json_type -> 'a) (* Map *)
+  -> (Json_type.json_type -> 'a -> 'b) (* Reduce *)
+  -> 'b (* Initial element *)
+  -> 'b
+  (** Execute a Temporary View on data *)
+
+
